937531ecb37c52f0ef7c5c47c4b7b2181b689d65
visolerga <vsoler@gmail.com>
2024-05-31 12:12:34 +0200

    obtiene correctamente los elementos li, pero no saca los title

--- Scrapeame.py
+++ Scrapeame.py
@@ -17,34 +17,38 @@ def save_to_file(content, filename):
     try:
         with open(filename, 'w', encoding='utf-8') as file:
             file.write(content)
-        print(f"El contenido de la página web se ha guardado en {filename}")
+        print(f"El contenido se ha guardado en {filename}")
     except IOError as e:
         print(f"Error al escribir en el archivo: {e}")
 
 def extract_li_titles(content, filename):
     try:
         # Expresión regular para encontrar todos los elementos <li> y </li>
-        li_regex = re.compile(r'<li[^>]*?>(.*?)<\/li>', re.DOTALL)
+        li_regex = re.compile(r'<li>(.*?)li>', re.DOTALL)
         # Encontramos todos los elementos <li> y </li> usando la expresión regular
         li_elements = li_regex.findall(content)
+        # Guardamos los elementos <li> en un archivo para revisarlos
+        save_to_file('\n'.join(li_elements), filename)
+        print(f"Los elementos <li> se han guardado en {filename}")
+
         # Expresión regular para verificar si los elementos <li> contienen el atributo title
         title_regex = re.compile(r'title=\"([^"]*?)\"')
         # Guardamos los títulos en un archivo separados por nuevas líneas
-        with open(filename, 'w', encoding='utf-8') as file:
+        with open('Titulos.txt', 'w', encoding='utf-8') as file:
             for li_element in li_elements:
                 # Buscamos si el elemento <li> contiene el atributo title
                 title_match = title_regex.search(li_element)
                 if title_match:
                     title = title_match.group(1).strip()
                     file.write(title + '\n')
-        print(f"Los títulos se han guardado en {filename}")
+        print(f"Los títulos se han guardado en Titulos.txt")
     except Exception as e:
         print(f"Error al extraer los títulos: {e}")
 
 def main(url):
     webpage_content = fetch_webpage(url)
     if webpage_content:
-        extract_li_titles(webpage_content, 'Titulos.txt')
+        extract_li_titles(webpage_content, 'Tabla.txt')
 
 if __name__ == "__main__":
     url = 'https://www.chollometro.com/ofertas/cursos-gratis-de-photoshop-chatgpt-excel-java-php-python-after-effect-aws-wordpress-y-otros-udemy-1297415'
